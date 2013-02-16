d27d0696af3ea2563c73fa4a3745d198697d1e26
Norbert Schneider <Norbert-Schneider@gmx.de>
2013-02-16 13:24:51 +0100

    Added a text based test for the first five days.
    This test takes the output of the unchanged function as "master copy"
    and compares it to the actual output after the refactoring.

---
+++ src/test/java/Days.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public final class Days {
+
+    public static final String SEP = System.getProperty("line.separator");
+
+    public static final List<String> DAYS = Collections.unmodifiableList(Arrays.asList("OMGHAI!" + SEP +
+            "|                                          Name | Quality | Sell In |" + SEP +
+            "|                             +5 Dexterity Vest |      20 |      10 |" + SEP +
+            "|                                     Aged Brie |       0 |       2 |" + SEP +
+            "|                        Elixir of the Mongoose |       7 |       5 |" + SEP +
+            "|                    Sulfuras, Hand of Ragnaros |      80 |       0 |" + SEP +
+            "|     Backstage passes to a TAFKAL80ETC concert |      20 |      15 |" + SEP +
+            "|                            Conjured Mana Cake |       6 |       3 |" + SEP,
+
+            "|                                          Name | Quality | Sell In |" + SEP +
+            "|                             +5 Dexterity Vest |      18 |       8 |" + SEP +
+            "|                                     Aged Brie |       2 |       0 |" + SEP +
+            "|                        Elixir of the Mongoose |       5 |       3 |" + SEP +
+            "|                    Sulfuras, Hand of Ragnaros |      80 |       0 |" + SEP +
+            "|     Backstage passes to a TAFKAL80ETC concert |      22 |      13 |" + SEP +
+            "|                            Conjured Mana Cake |       4 |       1 |" + SEP,
+
+            "|                                          Name | Quality | Sell In |" + SEP +
+            "|                             +5 Dexterity Vest |      17 |       7 |" + SEP +
+            "|                                     Aged Brie |       4 |      -1 |" + SEP +
+            "|                        Elixir of the Mongoose |       4 |       2 |" + SEP +
+            "|                    Sulfuras, Hand of Ragnaros |      80 |       0 |" + SEP +
+            "|     Backstage passes to a TAFKAL80ETC concert |      23 |      12 |" + SEP +
+            "|                            Conjured Mana Cake |       3 |       0 |" + SEP,
+
+            "|                                          Name | Quality | Sell In |" + SEP +
+            "|                             +5 Dexterity Vest |      16 |       6 |" + SEP +
+            "|                                     Aged Brie |       6 |      -2 |" + SEP +
+            "|                        Elixir of the Mongoose |       3 |       1 |" + SEP +
+            "|                    Sulfuras, Hand of Ragnaros |      80 |       0 |" + SEP +
+            "|     Backstage passes to a TAFKAL80ETC concert |      24 |      11 |" + SEP +
+            "|                            Conjured Mana Cake |       1 |      -1 |" + SEP,
+
+            "|                                          Name | Quality | Sell In |" + SEP +
+            "|                             +5 Dexterity Vest |      15 |       5 |" + SEP +
+            "|                                     Aged Brie |       8 |      -3 |" + SEP +
+            "|                        Elixir of the Mongoose |       2 |       0 |" + SEP +
+            "|                    Sulfuras, Hand of Ragnaros |      80 |       0 |" + SEP +
+            "|     Backstage passes to a TAFKAL80ETC concert |      25 |      10 |" + SEP +
+            "|                            Conjured Mana Cake |       0 |      -2 |" + SEP,
+
+            "|                                          Name | Quality | Sell In |" + SEP +
+            "|                             +5 Dexterity Vest |      14 |       4 |" + SEP +
+            "|                                     Aged Brie |      10 |      -4 |" + SEP +
+            "|                        Elixir of the Mongoose |       0 |      -1 |" + SEP +
+            "|                    Sulfuras, Hand of Ragnaros |      80 |       0 |" + SEP +
+            "|     Backstage passes to a TAFKAL80ETC concert |      27 |       9 |" + SEP +
+            "|                            Conjured Mana Cake |       0 |      -3 |" + SEP));
+}
--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,12 +1,34 @@
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static junit.framework.Assert.assertEquals;
 
 public class GildedRoseTest {
 
-	@Test
-	public void testTheTruth() {
-		assertTrue(true);
-	}
+    @Test
+    public void shouldPrintSameOutputForFirstDay() throws IOException {
+        ByteArrayOutputStream out = interceptOutput();
+        GildedRose.main(null);
+        assertEquals(Days.DAYS.get(0), out.toString());
+    }
+
+    @Test
+    public void shouldPrintSameOutputForNextFiveDays() throws IOException {
+        GildedRose.main(null);
+        for (int day = 1; day <= 5; day++) {
+            ByteArrayOutputStream out = interceptOutput();
+            GildedRose.updateQuality();
+            GildedRose.printItemsOnDay();
+            assertEquals(Days.DAYS.get(day), out.toString());
+        }
+    }
+
+    private ByteArrayOutputStream interceptOutput() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        return out;
+    }
 }
